b3df8966b5895eb882b910674c8a82f774e7b992
Dharaneesh Panneer Selvam <dharaneesh23110466@snuchennai.edu.in>
2025-11-15 12:58:45 +0530

    Updated comments

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ class AmbedkarGPT:
             num_thread=None,
         )
         
-        # Create a custom prompt template
+        # Custom prompt template
         prompt_template = """Use the following pieces of context to answer the question at the end. 
 If you don't know the answer based on the context provided, just say that you don't know, don't try to make up an answer.
 
